5260ec14d4e1f756bc4959c3c663899eefb970f3
ParkChanWoo <ParkChanWoo@DESKTOP-VID7PON>
2019-07-01 17:51:34 +0900

    a

--- Yeogiyo/src/main/java/com/sp/test/TestController.java
+++ Yeogiyo/src/main/java/com/sp/test/TestController.java
@@ -2,12 +2,13 @@ package com.sp.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("test.testController")
 public class TestController {
 	
-	@RequestMapping(value="/main/test")
+	@RequestMapping(value="/admin", method=RequestMethod.GET)
 	public String test() {
-		return ".user.test.test";
+		return ".userLayout";
 	}
 }

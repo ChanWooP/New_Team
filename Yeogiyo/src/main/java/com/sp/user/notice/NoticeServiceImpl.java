c79d852ea2b8f6d8dcaa07983b06d2076aaf3d53
xogml1226 <xogml1226@naver.com>
2019-07-10 14:26:08 +0900

    이태희

---
+++ Yeogiyo/src/main/java/com/sp/user/notice/NoticeServiceImpl.java
@@ -0,0 +1,60 @@
+package com.sp.user.notice;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service("user.notice.noticeService")
+public class NoticeServiceImpl implements NoticeService {
+
+	private co
+	@Override
+	public void insertNotice(Notice dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int dataCount(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Notice> listNotice(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Notice readNotice(int noticeNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Notice preReadNotice(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Notice nextReadNotice(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateNotice(Notice dto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteNotice(int noticeNum) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

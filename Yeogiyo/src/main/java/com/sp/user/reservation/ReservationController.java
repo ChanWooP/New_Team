afab2d87eeb07719a8ca3c33ff8387ff5f85c61b
kjhoon1994 <47774697+kjhoon1994@users.noreply.github.com>
2019-07-15 17:54:05 +0900

    재훈

--- Yeogiyo/src/main/java/com/sp/user/reservation/ReservationController.java
+++ Yeogiyo/src/main/java/com/sp/user/reservation/ReservationController.java
@@ -1,14 +1,41 @@
 package com.sp.user.reservation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("reservation.reservationController")
 public class ReservationController {
+		
+	@Autowired
+	private ReservationService service;
 	
-	
-	@RequestMapping(value="/user/reservation/reservation")
-	public String reservate() {
+	@RequestMapping(value="/user/reservation/reservation", method=RequestMethod.POST)
+	public String reservate(@RequestParam String hotelId, @RequestParam String roomtype, @RequestParam int roomprice,
+			@RequestParam int maxpeople, @RequestParam String roomdetails, Model model) {
+		
+		Map<String, Object> map = new HashMap<>();
+		int roomnum=0;
+		Reservation resdetail = null;
+		map.put("hotelId", hotelId);
+		map.put("roomtype", roomtype);
+		map.put("roomprice", roomprice);
+		map.put("roomdetails", roomdetails);
+		map.put("maxpeople", maxpeople);
+		map.put("roomstatus", "예약가능");
+		
+		roomnum = service.roomnum(map);
+		
+		resdetail=service.reservationdetail(roomnum);
+		
+		model.addAttribute("resdetail",resdetail);
+		
 		return ".user.reservation.reservation";
 	}
 	

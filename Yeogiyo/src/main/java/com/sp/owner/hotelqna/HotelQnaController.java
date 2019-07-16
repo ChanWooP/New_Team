3279f855e6f33d6e1df76142281e382727ad2905
ChanWooP <cksdn4002@gmail.com>
2019-07-16 17:48:29 +0900

    qna

--- Yeogiyo/src/main/java/com/sp/owner/hotelqna/HotelQnaController.java
+++ Yeogiyo/src/main/java/com/sp/owner/hotelqna/HotelQnaController.java
@@ -1,13 +1,94 @@
 package com.sp.owner.hotelqna;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sp.common.MyUtil;
 
 @Controller("owner.hotelqna.hotelQnaServiec")
 public class HotelQnaController {
 	
+	@Autowired
+	private MyUtil myUtil;
+	
+	@Autowired
+	private HotelQnaService service;
+	
 	@RequestMapping(value="/owner/hotelqna/list")
-	public String list() {
+	public String list(	@RequestParam(value="page", defaultValue="1") int current_page,
+			@RequestParam(defaultValue="all")String condition,
+			@RequestParam(defaultValue="") String keyword,
+			HttpServletRequest req,
+			Model model) throws Exception{
+		
+		int rows = 10;
+		int total_page = 0;
+		int dataCount = 0;
+		
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			keyword = URLDecoder.decode(keyword, "utf-8");
+		}
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("condition", condition);
+		map.put("keyword", keyword);
+		
+		dataCount = service.dataCount(map);
+		if(dataCount != 0) {
+			total_page = myUtil.pageCount(rows, dataCount);
+		}
+		
+		if(total_page < current_page) {
+			current_page = total_page;
+		}
+		
+		int start = (current_page - 1) * rows + 1;
+		int end = current_page * rows;
+		map.put("start", start);
+		map.put("end", end);
+		
+		List<HotelQna> list = service.listHotelQna(map);
+		
+        for(HotelQna dto : list) {
+        	dto.setQnaCreated(dto.getQnaCreated().substring(0, 10));
+        }
+		
+		String cp = req.getContextPath();
+		String query = "";
+		String listUrl = cp+"/owner/hotelqna/list";
+		
+		if(keyword.length()!=0) {
+			query = "condition="+condition+
+					"&keyword="+URLEncoder.encode(keyword, "utf-8");
+		}
+		
+		if(query.length()!=0) {
+			listUrl = cp+"/owner/hotelqna/list?"+query;
+		}
+		
+		String paging = myUtil.paging(current_page, total_page, listUrl);
+		
+		model.addAttribute("list", list);
+		model.addAttribute("page", current_page);
+		model.addAttribute("dataCount", dataCount);
+		model.addAttribute("total_page", total_page);
+		model.addAttribute("paging", paging);
+		model.addAttribute("condition", condition);
+		model.addAttribute("keyword", keyword);
+		
 		return ".owner.hotelqna.list";
 	}
 }
--- Yeogiyo/src/main/webapp/WEB-INF/view/owner/hotelqna/list.jsp
+++ Yeogiyo/src/main/webapp/WEB-INF/view/owner/hotelqna/list.jsp
@@ -19,17 +19,44 @@
 			<form name="searchForm" action="" method="post">
 				<select name="codition" style="height:30px;">
 					<option value="all" ${condition=="all"?"selected='selected'":"" }>모두</option>
-					<option value="reviewTitle" ${condition=="reviewTitle"?"selected='selected'":"" }>제목</option>
-					<option value="reviewContent" ${condition=="reviewContent"?"selected='selected'":"" }>내용</option>
+					<option value="qnaTitle" ${condition=="qnaTitle"?"selected='selected'":"" }>제목</option>
+					<option value="qnaCreated" ${condition=="qnaCreated"?"selected='selected'":"" }>내용</option>
 					<option value="userId" ${condition=="userId"?"selected='selected'":"" }>아이디</option>
 				</select>
- 				<input type="text" name="keyword" style="height:30px;" placeholder="리뷰 검색" value="${keyword }">
+ 				<input type="text" name="keyword" style="height:30px;" placeholder="문의사항 검색" value="${keyword }">
  				<button type="button" class="btn" onclick="hotSearch()">검색</button>
 			</form>
 			</td>
 		</tr>
 	</table>
-	<div>
-	
+	<div style="width:100%;" >
+		<table style="width:100%; margin-left:auto; margin-right:auto;" >
+			<tr style="border-bottom: 2px solid gray;">
+				<th style="width:10%">번호</th>
+				<th style="width:70%">제목</th>
+				<th style="width:10%">작성자</th>
+				<th style="width:10%">작성일</th>
+			</tr>
+			<c:forEach var="dto" items="${list }" >
+			<tr style="border-bottom: 1px solid gray;">
+				<td style="padding:3px;">${dto.rnum }</td>
+				<td style="padding:3px;"><a href="#">${dto.qnaTitle }</a></td>
+				<td style="padding:3px;">${dto.userId }</td>
+				<td style="padding:3px;">${dto.qnaCreated }</td>
+			</tr>
+			</c:forEach>
+		</table>
 	</div>
+	<table style="width:100%; margin-bottom:15px;">
+		<tr>
+			<td align="center">
+				<c:if test="${dataCount==0 }">
+					등록된 호텔 명소가 없습니다.
+				</c:if>
+				<c:if test="${dataCount!=0 }">
+					${paging }
+				</c:if>
+			</td>
+		</tr>
+	</table>
 </div>
